7e5f232ed5479d799537cea9e98305b270bf21b9
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-01-04 04:47:06 +1100

    add space to top ads

--- scraper/scraper/spiders/CarSpider.py
+++ scraper/scraper/spiders/CarSpider.py
@@ -111,7 +111,7 @@ class CarPriceSpider(scrapy.Spider):
                 continue
 
             # check constraints
-            if price < 1000:
+            if price < 3500 or price > 10000000:
                 continue
 
             year_filter = self.available[cars['make_name']][cars['model_name']]['year_filter']
